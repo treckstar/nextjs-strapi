2a6a7644709edf24e788c232cdc260c2ca2065d0
treckstar <treckstar@gmail.com>
2023-07-03 03:14:38 -0700

    fix: update strapi backend v4.11.3 & nodemailer

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,27 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT"),
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: {
+//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF"), // For self-signed certificates
+//       },
+//     },
+//   },
+// });
+
 const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 
+'.tmp/data2.db')),
     },
     useNullAsDefault: true,
   },
