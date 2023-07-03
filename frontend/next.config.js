2a17ec1f9a3f4fd006c0537731a656f2a28f1896
treckstar <treckstar@gmail.com>
2023-07-03 03:15:56 -0700

    fix: add nextui v2 beta, rem src dir

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  transpilePackages: ["@nextui-org/react", "@nextui-org/theme"],
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
   images: {
+    domains: [
+      "opencollective-production.s3.us-west-1.amazonaws.com",
+      "avatars.githubusercontent.com",
+      "logo.clearbit.com",
+      "i.pravatar.cc",
+    ],
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
+        hostname: 'nextjs-strapi.test',
+        port: '2337',
         pathname: '/uploads/**',
       },
       {
